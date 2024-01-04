bfb94eff47097bdc28817f455016ce545c222a60
DMV-Nicolas <dmvnicolas@gmail.com>
2024-01-03 21:18:57 -0500

    chess constants was added

---
+++ 02-chess/src/constants.ts
@@ -0,0 +1,30 @@
+export const PIECES = {
+    White: {
+        Pawn: "WPawn.svg",
+        Rook: "WRook.svg",
+        Horse: "WHorse.svg",
+        Bishop: "WBishop.svg",
+        Queen: "WQueen.svg",
+        King: "WKing.svg",
+    },
+    Black: {
+        Pawn: "BPawn.svg",
+        Rook: "BRook.svg",
+        Horse: "BHorse.svg",
+        Bishop: "BBishop.svg",
+        Queen: "BQueen.svg",
+        King: "BKing.svg",
+    },
+    Empty: "",
+}
+
+export const DEFAULT_BOARD = [
+    PIECES.Black.Rook, PIECES.Black.Horse, PIECES.Black.Bishop, PIECES.Black.Queen, PIECES.Black.King, PIECES.Black.Bishop, PIECES.Black.Horse, PIECES.Black.Rook,
+    PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn,
+    PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty,
+    PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty,
+    PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty,
+    PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty,
+    PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn,
+    PIECES.White.Rook, PIECES.White.Horse, PIECES.White.Bishop, PIECES.White.Queen, PIECES.White.King, PIECES.White.Bishop, PIECES.White.Horse, PIECES.White.Rook,
+]
