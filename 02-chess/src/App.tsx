67a4d631964787d17ed138fddf949d4053a9c96c
DMV-Nicolas <dmvnicolas@gmail.com>
2024-01-03 21:21:23 -0500

    handleSquareClick was added for test getPieceMoves

--- 02-chess/src/App.tsx
+++ 02-chess/src/App.tsx
@@ -1,9 +1,38 @@
 import { useState } from "react"
 
+import { getPieceMoves } from "./getPieceMoves"
+import { DEFAULT_BOARD, PIECES } from "./constants"
+
 import "./App.css"
 
 function App() {
-  const [board, setBoard] = useState(Array(64).fill(""))
+  const [board, setBoard] = useState(DEFAULT_BOARD)
+
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    console.log("HOLA")
+    if (e.target instanceof HTMLDivElement) {
+      const newBoard = [...board]
+      const moves = getPieceMoves(PIECES.White.Pawn, Number(e.target.dataset.index), newBoard)
+
+      for (const move of moves) {
+        newBoard[move] = "https://images.vexels.com/media/users/3/139158/isolated/preview/c862a3c9ef219140fb365301f9ebbd50-punto-negro.png"
+      }
+
+      setBoard(newBoard)
+    } else if (e.target instanceof HTMLImageElement) {
+      const parentElement = e.target.parentElement
+      if (parentElement instanceof HTMLDivElement) {
+        const newBoard = [...board]
+        const moves = getPieceMoves(PIECES.White.Pawn, Number(parentElement.dataset.index), newBoard)
+
+        for (const move of moves) {
+          newBoard[move] = "https://images.vexels.com/media/users/3/139158/isolated/preview/c862a3c9ef219140fb365301f9ebbd50-punto-negro.png"
+        }
+
+        setBoard(newBoard)
+      }
+    }
+  }
 
   let squareClassNameIsWhite = false
   return (
@@ -20,7 +49,7 @@ function App() {
                 <div
                   className={`square ${squareClassNameIsWhite ? "white" : "green"}`}
                   data-index={i}
-                //onClick={(e) => { handleClick(e) }}
+                  onClick={(e) => { handleClick(e) }}
                 >
                   <img src={cell} />
                 </div>
