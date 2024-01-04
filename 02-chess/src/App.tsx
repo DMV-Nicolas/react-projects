5034d57d37bf47298d5af6ff01cbaa3dcb76a5c4
DMV-Nicolas <dmvnicolas@gmail.com>
2024-01-03 22:41:11 -0500

    board was updated to a 2d board

--- 02-chess/src/App.tsx
+++ 02-chess/src/App.tsx
@@ -1,58 +1,37 @@
 import { useState } from "react"
 
-import { getPieceMoves } from "./getPieceMoves"
-import { DEFAULT_BOARD, PIECES } from "./constants"
+import { DEFAULT_BOARD } from "./constants"
 
 import "./App.css"
 
 function App() {
-  DEFAULT_BOARD[45] = PIECES.White.Bishop
-  DEFAULT_BOARD[35] = PIECES.Black.Bishop
   const [board, setBoard] = useState(DEFAULT_BOARD)
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    if (e.target instanceof HTMLDivElement) {
-      const newBoard = [...board]
-      const moves = getPieceMoves(PIECES.Black.Rook, Number(e.target.dataset.index), newBoard)
-
-      for (const move of moves) {
-        newBoard[move] = "https://images.vexels.com/media/users/3/139158/isolated/preview/c862a3c9ef219140fb365301f9ebbd50-punto-negro.png"
-      }
-
-      setBoard(newBoard)
-    } else if (e.target instanceof HTMLImageElement) {
-      const parentElement = e.target.parentElement
-      if (parentElement instanceof HTMLDivElement) {
-        const newBoard = [...board]
-        const moves = getPieceMoves(PIECES.Black.Rook, Number(parentElement.dataset.index), newBoard)
-
-        for (const move of moves) {
-          newBoard[move] = "https://images.vexels.com/media/users/3/139158/isolated/preview/c862a3c9ef219140fb365301f9ebbd50-punto-negro.png"
-        }
-
-        setBoard(newBoard)
-      }
-    }
+    console.log(e)
   }
 
-  let squareClassNameIsWhite = false
   return (
     <>
       <main className="game">
         <div className="board">
           {
-            board.map((cell, i) => {
-              if (i % 8 != 0) {
-                squareClassNameIsWhite = !squareClassNameIsWhite
-              }
-
+            board.map((row, i) => {
               return (
-                <div
-                  className={`square ${squareClassNameIsWhite ? "white" : "green"}`}
-                  data-index={i}
-                  onClick={(e) => { handleClick(e) }}
-                >
-                  <img src={cell} />
+                <div className="row">
+                  {
+                    row.map((cell, j) => {
+                      return (
+                        <div
+                          className={`square ${(i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1) ? "white" : "green"}`}
+                          data-index={(8 * i) + j}
+                          onClick={(e) => { handleClick(e) }}
+                        >
+                          <img src={cell} />
+                        </div>
+                      )
+                    })
+                  }
                 </div>
               )
             })
--- 02-chess/src/constants.ts
+++ 02-chess/src/constants.ts
@@ -19,12 +19,12 @@ export const PIECES = {
 }
 
 export const DEFAULT_BOARD = [
-    PIECES.Black.Rook, PIECES.Black.Horse, PIECES.Black.Bishop, PIECES.Black.Queen, PIECES.Black.King, PIECES.Black.Bishop, PIECES.Black.Horse, PIECES.Black.Rook,
-    PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn,
-    PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty,
-    PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty,
-    PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty,
-    PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty,
-    PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn,
-    PIECES.White.Rook, PIECES.White.Horse, PIECES.White.Bishop, PIECES.White.Queen, PIECES.White.King, PIECES.White.Bishop, PIECES.White.Horse, PIECES.White.Rook,
+    [PIECES.Black.Rook, PIECES.Black.Horse, PIECES.Black.Bishop, PIECES.Black.Queen, PIECES.Black.King, PIECES.Black.Bishop, PIECES.Black.Horse, PIECES.Black.Rook],
+    [PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn, PIECES.Black.Pawn],
+    [PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty],
+    [PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty],
+    [PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty],
+    [PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty, PIECES.Empty],
+    [PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn, PIECES.White.Pawn],
+    [PIECES.White.Rook, PIECES.White.Horse, PIECES.White.Bishop, PIECES.White.Queen, PIECES.White.King, PIECES.White.Bishop, PIECES.White.Horse, PIECES.White.Rook]
 ]
