92e407f20b8ad4d36e737d36f39bc514c033543b
DMV-Nicolas <dmvnicolas@gmail.com>
2024-01-21 14:52:30 -0500

    little changes

--- 08-translate/src/App.test.tsx
+++ 08-translate/src/App.test.tsx
@@ -1,4 +1,4 @@
-import { describe, it, expect, test } from "vitest"
+import { expect, test } from "vitest"
 import { render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import App from "./App"
