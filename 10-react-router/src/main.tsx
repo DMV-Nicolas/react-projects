37b2f4d9df287dc4884af22e61dc53666461ea8c
DMV-Nicolas <dmvnicolas@gmail.com>
2024-01-22 17:58:05 -0500

    rice

--- 10-react-router/src/main.tsx
+++ 10-react-router/src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = document.getElementById('root')
 if (root instanceof HTMLElement) {
   ReactDOM.createRoot(root).render(
-    <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>,
+    </BrowserRouter>,
   )
 }
